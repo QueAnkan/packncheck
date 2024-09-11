a5339ca8a0660048473261ad9821209300598ced
QueAnka <annika.nylin@hotmail.com>
2024-09-11 22:56:57 +0200

    added gradient borders

--- src/app/add-lists/page.tsx
+++ src/app/add-lists/page.tsx
@@ -17,6 +17,11 @@ const AddListsPage = () => {
         isSaved &&
         "absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 text-white text-2xl";
 
+    const disabledButton =
+        !newList.title || !newList.checkItems.length
+            ? "bg-gradient opacity-50 text-black font-bold w-fit px-4 py-1 rounded-3xl"
+            : "text-black font-bold bg-gradient w-fit px-4 py-1 rounded-3xl";
+
     const addedListMessage = () => {
         setIsSaved(true);
         setTimeout(() => {
@@ -64,7 +69,10 @@ const AddListsPage = () => {
         setNewCheckItem("");
     };
 
-    const handleDeleteItem = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>,index: number) => {
+    const handleDeleteItem = (
+        event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+        index: number
+    ) => {
         event.preventDefault();
         setNewList({
             ...newList,
@@ -78,118 +86,127 @@ const AddListsPage = () => {
     return (
         <div className='h-full flex flex-col items-center gap-4 p-8'>
             <h1 className='text-3xl'>Skapa ny lista</h1>
-            <form
-                action='/'
-                className='relative flex flex-col justify-start gap-y-10 border rounded-sm w-full h-full mx-auto p-4'
-            >
-                <div className='flex flex-col gap-y-8'>
-                    <div className='flex items-center'>
-                        <label
-                            htmlFor='ListName'
-                            className='flex flex-col w-3/5'
-                        >
-                            Titel
-                            <input
-                                id='ListName'
-                                type='text'
-                                value={newList.title}
-                                onChange={(
-                                    event: React.ChangeEvent<HTMLInputElement>
-                                ) =>
-                                    setNewList({
-                                        ...newList,
-                                        title: event.target.value,
-                                    })
-                                }
-                                className='rounded-md bg-[#F8F8CA] text-black'
-                            />
-                        </label>
+            <div className="bg-gradient p-px">
+                <form
+                    action='/'
+                    className='relative flex flex-col justify-start gap-y-10 bg-black rounded-sm w-full h-full mx-auto p-4'
+                >
+                    <div className='flex flex-col gap-y-8'>
+                        <div className='flex items-center'>
+                            <label
+                                htmlFor='ListName'
+                                className='flex flex-col w-3/5'
+                            >
+                                Titel
+                                <input
+                                    id='ListName'
+                                    type='text'
+                                    value={newList.title}
+                                    onChange={(
+                                        event: React.ChangeEvent<HTMLInputElement>
+                                    ) =>
+                                        setNewList({
+                                            ...newList,
+                                            title: event.target.value,
+                                        })
+                                    }
+                                    className='rounded-md bg-[#F8F8CA] text-black'
+                                />
+                            </label>
+                        </div>
+
+                        <div className='flex items-end justify-start gap-1'>
+                            <label
+                                htmlFor='checkItem'
+                                className='flex flex-col w-3/5'
+                            >
+                                Ny uppgift
+                                <input
+                                    id='checkItem'
+                                    type='text'
+                                    value={newCheckItem}
+                                    onChange={(
+                                        event: React.ChangeEvent<HTMLInputElement>
+                                    ) => setNewCheckItem(event.target.value)}
+                                    className=' rounded-md bg-[#F8F8CA] text-black'
+                                />
+                            </label>
+                            <button
+                                className=' flex items-center gap-x-0.5'
+                                disabled={!newCheckItem}
+                                onClick={handleAddItem}
+                                type='button'
+                            >
+                                <Image
+                                    alt='Lägg till namn på listan'
+                                    src='/typcn_plus.svg'
+                                    width={10}
+                                    height={10}
+                                ></Image>
+                                <p className='text-nowrap text-sm'>Lägg till</p>
+                            </button>
+                        </div>
                     </div>
 
-                    <div className='flex items-end justify-start gap-1'>
-                        <label
-                            htmlFor='checkItem'
-                            className='flex flex-col w-3/5'
-                        >
-                            Ny uppgift
-                            <input
-                                id='checkItem'
-                                type='text'
-                                value={newCheckItem}
-                                onChange={(
-                                    event: React.ChangeEvent<HTMLInputElement>
-                                ) => setNewCheckItem(event.target.value)}
-                                className=' rounded-md bg-[#F8F8CA] text-black'
-                            />
-                        </label>
+                    <div className=' w-full h-0 bottom-12 left-0 flex  flex-col items-end overflow-visible'>
                         <button
-                            className=' flex items-center gap-x-0.5'
-                            disabled={!newCheckItem}
-                            onClick={handleAddItem}
-                            type='button'
+                            type='submit'
+                            className={`${disabledButton}`}
+                            onClick={(
+                                event: React.MouseEvent<HTMLButtonElement>
+                            ) => handleSubmit(event)}
                         >
-                            <Image
-                                alt='Lägg till namn på listan'
-                                src='/typcn_plus.svg'
-                                width={10}
-                                height={10}
-                            ></Image>
-                            <p className='text-nowrap text-sm'>Lägg till</p>
+                            Spara lista
                         </button>
                     </div>
-                </div>
-
-                <div className=' w-full h-0 bottom-12 left-0 flex  flex-col items-end overflow-visible'>
-                    <button
-                        type='submit'
-                        className='text-black font-bold bg-gradient w-fit px-4 py-1 rounded-3xl '
-                        onClick={(event: React.MouseEvent<HTMLButtonElement>) =>
-                            handleSubmit(event)
-                        }
-                    >
-                        Spara lista
-                    </button>
-                </div>
-                <div className='flex flex-col items-start'>
-                    <h2 className=''> {newList.title}</h2>
-                    <ul className='w-full'>
-                        {newList.checkItems.map((listItem, index: number) => {
-                            return (
-                                <li
-                                    key={index}
-                                    className='flex justify-between w-1/2 my-2 '
-                                >
-                                    <div className='flex gap-4'>
-                                        <input
-                                            type='checkbox'
-                                            id='checkItem'
-                                            name='checkItem'
-                                        ></input>
-                                        <p>{listItem}</p>
-                                    </div>
-                                    <button
-                                        className='justify-self-end'
-                                        onClick={(event) => handleDeleteItem(event, index)}
-                                    >
-                                        <Image
-                                            alt='Ta bort uppgift'
-                                            src='/bytesize_trash.svg'
-                                            width={20}
-                                            height={20}
-                                        ></Image>
-                                    </button>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
-
-                <div className={`${messageClasses}`}>
-                    <div className='bg-black px-8 py-12 rounded-sm'>
-                        {isSaved && <p>Listan är sparad</p>}
+                    <div className='flex flex-col items-start'>
+                        <h2 className=''> {newList.title}</h2>
+                        <ul className='w-full'>
+                            {newList.checkItems.map(
+                                (listItem, index: number) => {
+                                    return (
+                                        <li
+                                            key={index}
+                                            className='flex justify-between w-1/2 my-2 '
+                                        >
+                                            <div className='flex gap-4'>
+                                                <input
+                                                    type='checkbox'
+                                                    id='checkItem'
+                                                    name='checkItem'
+                                                ></input>
+                                                <p>{listItem}</p>
+                                            </div>
+                                            <button
+                                                className='justify-self-end'
+                                                onClick={(event) =>
+                                                    handleDeleteItem(
+                                                        event,
+                                                        index
+                                                    )
+                                                }
+                                            >
+                                                <Image
+                                                    alt='Ta bort uppgift'
+                                                    src='/bytesize_trash.svg'
+                                                    width={20}
+                                                    height={20}
+                                                ></Image>
+                                            </button>
+                                        </li>
+                                    );
+                                }
+                            )}
+                        </ul>
                     </div>
-                </div>
-            </form>
+
+                    <div className={`${messageClasses}`}>
+                        <div className='bg-black px-8 py-12 rounded-sm'>
+                            {isSaved && <p>Listan är sparad</p>}
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
--- src/app/see-all-lists/page.tsx
+++ src/app/see-all-lists/page.tsx
@@ -8,7 +8,7 @@ const SeeAllLists = () => {
     const [lists, setLists] = useState<any[]>([]);
     const [isOpen, setIsOpen] = useState<null | number>(null);
 
-    const visibleClass = isOpen !== null ? "visible" : "hidden";
+    const visibleClass = isOpen !== null ? "visible bg-gradient p-0.5 rounded-3xl" : "hidden";
 
     const storedLists = JSON.parse(localStorage.getItem("myLists") || "[]");
     console.log(storedLists);
@@ -29,7 +29,7 @@ const SeeAllLists = () => {
 
     return (
         <div className=' h-screen flex flex-col justify-start items-center gap-y-8 py-8 '>
-            <h1 className='text-3xl'>See All Lists</h1>
+            <h1 className='text-3xl'>Välj lista</h1>
             <ul className='flex flex-wrap justify-evenly gap-y-8 '>
                 {storedLists.map(
                     (
@@ -38,61 +38,69 @@ const SeeAllLists = () => {
                         index: number
                     ) => (
                         <li
-                            className='relative flex items-center justify-between border border-white rounded-3xl w-56 text-center py-2 px-4'
-                            key={index+list.title}
+                            className='relative flex items-center justify-between bg-gradient p-px rounded-3xl w-56 text-center '
+                            key={index + list.title}
                         >
-                            <div
-                                className=' cursor-pointer w-full h-full text-left z-10 border border-red-300'
-                                onClick={() =>
-                                    setIsOpen(isOpen === index ? null : index)
-                                }
-                            >
-                                <h2 className='text-xl font-semibold'>
-                                    {list.title}
-                                </h2>
-                            </div>
-                            <button
-                                className='p-2 border border-blue-300'
-                                onClick={() => handleDeleteList(index)}
-                            >
-                                <Image
-                                    alt='Ta bort uppgift'
-                                    src='/bytesize_trash.svg'
-                                    width={20}
-                                    height={20}
-                                ></Image>
-                            </button>
-                            <div className='absolute w-full left-0  bg-black rounded-3xl  z-50'>
-                                <div className={`${visibleClass}`}>
-                                    {isOpen === index && (
-                                        <ul className='w-full h-fit border border-white py-2 px-4 rounded-3xl min-h-60'>
-                                            <button
-                                                onClick={() => {
-                                                    closeList(index);
-                                                }}
-                                            >
-                                                X
-                                            </button>
-                                            {list.checkItems.map(
-                                                (
-                                                    item: string,
-                                                    index: number
-                                                ) => (
-                                                    <li
-                                                        key={index}
-                                                        className='flex gap-4 pb-4'
+                            <div className='flex items-center justify-between w-full py-2 px-4 bg-black rounded-3xl '>
+                                <div
+                                    className=' cursor-pointer w-full h-full text-left z-10'
+                                    onClick={() =>
+                                        setIsOpen(
+                                            isOpen === index ? null : index
+                                        )
+                                    }
+                                >
+                                    <h2 className='text-xl font-semibold'>
+                                        {list.title}
+                                    </h2>
+                                </div>
+                                <button
+                                    className='p-2'
+                                    onClick={() => handleDeleteList(index)}
+                                >
+                                    <Image
+                                        alt='Ta bort lista'
+                                        src='/bytesize_trash.svg'
+                                        width={20}
+                                        height={20}
+                                    ></Image>
+                                </button>
+                                <div className='absolute w-full left-0  bg-black rounded-3xl  z-50'>
+                                    <div className={`${visibleClass}`}>
+                                        {isOpen === index && (
+                                            <ul className='w-full h-fit bg-black py-2 px-4 rounded-3xl min-h-60'>
+                                                <div className='w-full flex '>
+                                                    <span className='w-full'></span>
+                                                    <button
+                                                        className='h-full rounded-full py-1 px-3 bg-gradient-radial from-slate-400 via-slate-800 to-black'
+                                                        onClick={() => {
+                                                            closeList(index);
+                                                        }}
                                                     >
-                                                        <input
-                                                            type='checkbox'
-                                                            id='checkItem'
-                                                            name='checkItem'
-                                                        ></input>
-                                                        <p>{item}</p>
-                                                    </li>
-                                                )
-                                            )}
-                                        </ul>
-                                    )}
+                                                        X
+                                                    </button>
+                                                </div>
+                                                {list.checkItems.map(
+                                                    (
+                                                        item: string,
+                                                        index: number
+                                                    ) => (
+                                                        <li
+                                                            key={index}
+                                                            className='flex gap-4 pb-4'
+                                                        >
+                                                            <input
+                                                                type='checkbox'
+                                                                id='checkItem'
+                                                                name='checkItem'
+                                                            ></input>
+                                                            <p>{item}</p>
+                                                        </li>
+                                                    )
+                                                )}
+                                            </ul>
+                                        )}
+                                    </div>
                                 </div>
                             </div>
                         </li>
