88ad007cc8b7f81683de1a323f23ac58ff720a81
QueAnka <annika.nylin@hotmail.com>
2024-09-20 14:55:39 +0200

    changed filename for navigation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
-import Navigation from "./Navigation";
+import Navigation from "./ListLinks";
 
-const Footer = () => {	
-	return (
-		<footer className="fixed bottom-0 w-full lg:hidden">
-			<Navigation />
-		</footer>
-	)
-}
+const Footer = () => {
+    return (
+        <footer className='fixed bottom-0 w-full lg:hidden'>
+            <Navigation />
+        </footer>
+    );
+};
 
 export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { irishGrover } from "../app/layout";
 import Link from "next/link";
-import Navigation from "./Navigation";
+import Navigation from "./ListLinks";
 
 const Header = () => {
     return (
